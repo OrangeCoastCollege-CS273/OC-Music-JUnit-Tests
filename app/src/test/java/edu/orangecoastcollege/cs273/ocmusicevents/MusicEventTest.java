1e1b8462525f3770ca9473dc7687a8a8f555d1d4
Severiano Badajoz <sevebadajoz@gmail.com>
2017-11-30 15:19:06 -0800

    Create MusicEventTest.java

---
+++ app/src/test/java/edu/orangecoastcollege/cs273/ocmusicevents/MusicEventTest.java
@@ -0,0 +1,81 @@
+package edu.orangecoastcollege.cs273.ocmusicevents;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MusicEventTest {
+
+    private MusicEvent testEvent;
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void getTitle() throws Exception {
+
+    }
+
+    @Test
+    public void setTitle() throws Exception {
+    }
+
+    @Test
+    public void getDate() throws Exception {
+    }
+
+    @Test
+    public void setDate() throws Exception {
+    }
+
+    @Test
+    public void getDay() throws Exception {
+    }
+
+    @Test
+    public void setDay() throws Exception {
+    }
+
+    @Test
+    public void getTime() throws Exception {
+    }
+
+    @Test
+    public void setTime() throws Exception {
+    }
+
+    @Test
+    public void getLocation() throws Exception {
+    }
+
+    @Test
+    public void setLocation() throws Exception {
+    }
+
+    @Test
+    public void getAddress1() throws Exception {
+    }
+
+    @Test
+    public void setAddress1() throws Exception {
+    }
+
+    @Test
+    public void getAddress2() throws Exception {
+    }
+
+    @Test
+    public void setAddress2() throws Exception {
+    }
+
+    @Test
+    public void getImageName() throws Exception {
+    }
+
+    @Test
+    public void setImageName() throws Exception {
+    }
+
+}
